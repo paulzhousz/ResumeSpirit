ee6251a15640afff5dfc4092f1052ccf80851048
Paul Zhou <paulzhousz@gmail.com>
2016-03-03 14:47:27 +0800

    Design Doc/ResumeSprit.ldm

--- models/config.py
+++ models/config.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+
 __author__ = 'Paul'
 
 '''
@@ -8,3 +10,7 @@ __author__ = 'Paul'
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+db_url = "sqlite:///" + os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, "app.db"))
+engine = create_engine(db_url)
+db_session = sessionmaker(bind=engine)
---
+++ models/position.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Paul'
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Position(Base):
+    __tablename__ = "T_PositionInfo"
+
+    positionID = Column(String(50), primary_key=True)
+    positionname = Column(String(100))
+    hiringnumber = Column(Integer)
+    location = Column(String(50))
+    workingtime = Column(String(20))
+    degree = Column(String(20))
+    sex = Column(String(10))
+    language = Column(String(20))
+    languagelevel = Column(String(10))
+    agefrom = Column(Integer)
+    ageto = Column(Integer)
+    experience = Column(Integer)
+    category = Column(String(100))
+    major = Column(String(100))
+    salary = Column(String(100))
+    positiondesc = Column(String(2000))
+    enddate = Column(String(20))
+    source = Column(String(20))
+    sourcepositionid = Column(String(50))
+    status = Column(String(10))
+    createdate = Column(String(30))
+    updatedate = Column(String(30))

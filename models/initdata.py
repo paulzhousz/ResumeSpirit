b32017e1e4db661b260e0831edcfef618d20ea45
Paul Zhou <paulzhousz@gmail.com>
2016-03-04 22:43:11 +0800

    ~

--- models/initdata.py
+++ models/initdata.py
@@ -2,3 +2,18 @@
 # -*- coding: utf-8 -*-
 # @Date :'3/3/16 23:07'
 __author__ = 'paul'
+
+from models import config, sourcesiteinfo
+import uuid
+import datetime
+
+session = config.db_session()
+site_ohr = sourcesiteinfo.SourcesiteInfo(
+    sourceid=str(uuid.uuid1()),
+    code="OHR",
+    name="OHR",
+    createdate=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+)
+session.add(site_ohr)
+session.commit()
+session.close()

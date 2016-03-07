5854370b611ea390919bd70c060171ddbc1dde51
Paul Zhou <paulzhousz@gmail.com>
2016-03-07 17:57:02 +0800

    update resume model

--- Design Doc/resumespirit.sql
+++ Design Doc/resumespirit.sql
@@ -14,6 +14,26 @@ drop index T_ResumeInfo_PK;
 
 drop table T_ResumeInfo;
 
+drop index T_ResumeLanguage_PK;
+
+drop table T_ResumeLanguage;
+
+drop index T_ResumeProjectexp_PK;
+
+drop table T_ResumeProjectexp;
+
+drop index T_ResumeSkill_PK;
+
+drop table T_ResumeSkill;
+
+drop index T_ResumeTraining_PK;
+
+drop table T_ResumeTraining;
+
+drop index T_ResumeWorkingexp_PK;
+
+drop table T_ResumeWorkingexp;
+
 drop index T_SourceSiteInfo_PK;
 
 drop table T_SourceSiteInfo;
@@ -21,6 +41,10 @@ drop table T_SourceSiteInfo;
 drop index T_branchInfo_PK;
 
 drop table T_branchInfo;
+
+drop index T_resumeeducation_PK;
+
+drop table T_resumeeducation;
 
 drop index T_userInfo_PK;
 
@@ -95,6 +119,7 @@ resumeID             VARCHAR(50)                    not null,
 cname                VARCHAR(20),
 ename                VARCHAR(20),
 sex                  VARCHAR(10),
+Residence            VARCHAR(50),
 IDtype               VARCHAR(20),
 IDNumber             VARCHAR(50),
 birthdate            VARCHAR(20),
@@ -107,20 +132,24 @@ partisan             VARCHAR(100),
 hukou                VARCHAR(100),
 address              VARCHAR(100),
 degree               VARCHAR(50),
+WorkingStart         VARCHAR(10),
 currentSalary        VARCHAR(50),
 jobStatus            VARCHAR(50),
 overseaExp           VARCHAR(10),
+overseadesc          VARCHAR(500),
 avaliableTime        VARCHAR(20),
 jobTime              VARCHAR(10),
 expectIndustry       VARCHAR(100),
 expectSalary         VARCHAR(100),
 category             VARCHAR(100),
 EnglishLevel         VARCHAR(100),
+Japaneselevel        VARCHAR(100),
 selfEvaluation       VARCHAR(500),
 skill                VARCHAR(500),
 postTime             VARCHAR(30),
 Source               VARCHAR(20),
 sourceresumeID       VARCHAR(50),
+sourceurl            VARCHAR(100),
 Status               VARCHAR(10),
 CreateDate           VARCHAR(30),
 UpdateDate           VARCHAR(30),
@@ -129,6 +158,51 @@ primary key (resumeID)
 
 create unique index T_ResumeInfo_PK on T_ResumeInfo (
 resumeID ASC
+);
+
+create table T_ResumeLanguage (
+languageskillid      VARCHAR(50)                    not null,
+primary key (languageskillid)
+);
+
+create unique index T_ResumeLanguage_PK on T_ResumeLanguage (
+languageskillid ASC
+);
+
+create table T_ResumeProjectexp (
+projectexpid         VARCHAR(50)                    not null,
+primary key (projectexpid)
+);
+
+create unique index T_ResumeProjectexp_PK on T_ResumeProjectexp (
+projectexpid ASC
+);
+
+create table T_ResumeSkill (
+skillid              VARCHAR(50)                    not null,
+primary key (skillid)
+);
+
+create unique index T_ResumeSkill_PK on T_ResumeSkill (
+skillid ASC
+);
+
+create table T_ResumeTraining (
+educationid          VARCHAR(50)                    not null,
+primary key (educationid)
+);
+
+create unique index T_ResumeTraining_PK on T_ResumeTraining (
+educationid ASC
+);
+
+create table T_ResumeWorkingexp (
+workingexpid         VARCHAR(50)                    not null,
+primary key (workingexpid)
+);
+
+create unique index T_ResumeWorkingexp_PK on T_ResumeWorkingexp (
+workingexpid ASC
 );
 
 create table T_SourceSiteInfo (
@@ -155,6 +229,15 @@ primary key (BranchID)
 
 create unique index T_branchInfo_PK on T_branchInfo (
 BranchID ASC
+);
+
+create table T_resumeeducation (
+educationid          VARCHAR(50)                    not null,
+primary key (educationid)
+);
+
+create unique index T_resumeeducation_PK on T_resumeeducation (
+educationid ASC
 );
 
 create table T_userInfo (

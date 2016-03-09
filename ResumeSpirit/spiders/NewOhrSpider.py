e9383939aaada099f796d08f292314a50e3b8f0c
Paul Zhou <paulzhousz@gmail.com>
2016-03-09 15:28:01 +0800

    get resume data

--- ResumeSpirit/spiders/NewOhrSpider.py
+++ ResumeSpirit/spiders/NewOhrSpider.py
@@ -305,11 +305,11 @@ class NewOhrSpider(Spider):
                 urls = resume_url_list[i]
                 #: 处理简历详细页面url
                 resume_url = self.site_url + urls.split("'")[1]
-                self.log(resume_url)
+                # self.log(resume_url)
                 #: 获取简历投递日期
                 datepath = '//table//tr[' + str(i + 2) + ']/td[6]/text()'
                 post_time = sel.xpath(datepath).extract_first()
-                resumeitem["Source"] = "OHR"
+                resumeitem["source"] = "OHR"
                 resumeitem["postTime"] = post_time
                 resumeitem["sourcepositionid"] = response.meta["positioncode"]
                 #: 抓取简历详细页面
@@ -343,7 +343,115 @@ class NewOhrSpider(Spider):
     #: parse_resumedata(self,response)
     #: 处理简历详细页面
     def parse_resumedata(self, response):
-        resume_item=response.meta["resume_item"]
+        sel = Selector(response)
+        resume_item = response.meta["resume_item"]
+        resume_item["sourceurl"] = response.url
+        # 中文姓名
+        cname = sel.xpath('//div[@class="user_name"]/text()').extract_first(default="")
+        resume_item["cname"] = cname
+        # 英文姓名
+        ename = ""
+        resume_item["ename"] = ename
+        # 性别
+        sex = sel.xpath('//div[@class="fs14"]/text()').extract_first(default="")
+        resume_item["sex"] = sex
+        # 居住城市
+        residence = sel.xpath('//div[@class="tag"]/text()').extract_first(default="")
+        resume_item["residence"] = residence
+
+        # 基本资料 begin
+        # 表格第1行
+        # 证件类型
+        IDtype = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[1]/td[2]/text()').extract_first(default="")
+        # self.log(IDtype)
+        resume_item["IDtype"] = IDtype
+        # 证件号
+        IDNumber = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[1]/td[4]/text()').extract_first(default="")
+        # self.log(IDNumber)
+        resume_item["IDNumber"] = IDNumber
+
+        # 表格第2行
+        # 出生年月
+        birthdate = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[2]/td[2]/text()').extract_first(default="")
+        birthdate = birthdate[:7]
+        # self.log(birthdate)
+        resume_item["birthdate"] = birthdate
+        # 婚姻状况
+        marital = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[2]/td[4]/text()').extract_first(default="")
+        # self.log(marital)
+        resume_item["marital"] = marital
+
+        # 表格第3行
+        # 手机号码
+        mobile = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[3]/td[2]/text()').extract_first(default="")
+        # self.log(mobile)
+        resume_item["mobile"] = mobile
+        # 家庭电话
+        phoneNumber = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[3]/td[4]/text()').extract_first(default="")
+        # self.log(phoneNumber)
+        resume_item["phoneNumber"] = phoneNumber
+
+        # 表格第4行
+        # 邮箱
+        email = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[4]/td[2]/text()').extract_first(default="")
+        # self.log(email)
+        resume_item["email"] = email
+        # QQ
+        qqNumber = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[4]/td[4]/text()').extract_first(default="")
+        # self.log(qqNumber)
+        resume_item["qqNumber"] = qqNumber
+
+        # 表格第5行
+        # 党派
+        partisan = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[5]/td[2]/text()').extract_first(default="")
+        # self.log(partisan)
+        resume_item["partisan"] = partisan
+        # 户口
+        hukou = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[5]/td[4]/text()').extract_first(default="")
+        # self.log(hukou)
+        resume_item["hukou"] = hukou
+
+        # 表格第6行
+        # 地址
+        address = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[6]/td[2]/text()').extract_first(default="")
+        #self.log(address)
+        resume_item["address"] = address
+
+        # 表格第7行
+        # 学历
+        degree = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[7]/td[2]/text()').extract_first(default="")
+        # self.log(degree)
+        resume_item["degree"] = degree
+        # 工作起始年
+        workingstart = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[7]/td[4]/text()').extract_first(default="")
+        # self.log(WorkingStart)
+        resume_item["WorkingStart"] = workingstart
+
+        # 表格第8行
+        # 目前年薪
+        currentSalary = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[8]/td[2]/text()').extract_first(default="")
+        # self.log(currentSalary)
+        resume_item["currentSalary"] = currentSalary
+
+        # 表格第9行
+        # 求职状态
+        jobStatus = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[9]/td[2]/text()').extract_first(default="")
+        # self.log(jobStatus)
+        resume_item["jobStatus"] = jobStatus
+        # 海外经验
+        overseaExp = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[9]/td[4]/text()').extract_first(default="")
+        # self.log(overseaExp)
+        resume_item["overseaExp"] = overseaExp
+
+        # 表格第10行
+        # 海外经历
+        overseadesc = sel.xpath('//div[@id="p_info_view"]/table/tbody/tr[10]/td[2]/text()').extract_first(
+                default="")
+        # self.log(overseadesc)
+        resume_item["overseadesc"] = overseadesc
+        # 基本资料 end
+
+        # self.log(resume_item)
 
     #: get_pageNumber(self, selector):
     #: 从返回的html中获取数据页数
